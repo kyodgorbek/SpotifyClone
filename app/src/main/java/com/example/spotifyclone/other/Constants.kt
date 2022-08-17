9a0848ce73565701c81913680803cc84d84553c8
yodgor777 <kyodgorbek@gmail.com>
2022-08-17 11:47:29 +0200

    Implemented SongFragment.kt

--- app/src/main/java/com/example/spotifyclone/other/Constants.kt
+++ app/src/main/java/com/example/spotifyclone/other/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val NOTIFICATION_ID =  1
     const val NOTIFICATION_CHANNEL_ID = "music"
     const val MEDIA_ROOT_ID = "root_id"
+    const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
     const val NETWORK_ERROR = "NETWORK_ERROR"
 }
--- app/src/main/java/com/example/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/example/spotifyclone/ui/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.spotifyclone.ui
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
+import com.example.spotifyclone.R
 import com.example.spotifyclone.adapters.SwipeSongAdapter
 import com.example.spotifyclone.data.entities.Song
 import com.example.spotifyclone.databinding.ActivityMainBinding
+import com.example.spotifyclone.exoplayer.isPlaying
 import com.example.spotifyclone.exoplayer.toSong
 import com.example.spotifyclone.other.Status.*
 import com.example.spotifyclone.ui.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -19,6 +28,9 @@ class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController:NavController
+
+
 
     @Inject
     lateinit var glide: RequestManager
@@ -26,15 +38,62 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var swipeSongAdapter: SwipeSongAdapter
     private var curPlayingSong: Song? = null
+    private var playbackState: PlaybackStateCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
         subscribeToObservers()
         binding.vpSong.adapter = swipeSongAdapter
+        binding.vpSong.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(playbackState?.isPlaying == true){
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                }else{
+                    curPlayingSong = swipeSongAdapter.songs[position]
+                }
+            }
+        })
+        binding.ivPlayPause.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
 
+        swipeSongAdapter.setItemClickListener {
+            navController.navigate(
+                R.id.globalActionToSongFragment
+            )
+        }
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.songFragment-> hideBottomBar()
+                R.id.homeFragment -> showBottomBar()
+            }
+
+        }
+
+    }
+
+    private fun hideBottomBar(){
+        binding.ivCurSongImage.isVisible = false
+        binding.vpSong.isVisible = false
+        binding.ivPlayPause.isVisible = false
+    }
+
+    private fun showBottomBar(){
+        binding.ivCurSongImage.isVisible = true
+        binding.vpSong.isVisible = true
+        binding.ivPlayPause.isVisible = true
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
@@ -71,6 +130,39 @@ class MainActivity : AppCompatActivity() {
             curPlayingSong = it.toSong()
             glide.load(curPlayingSong?.imageUrl).into(binding.ivCurSongImage)
             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
+        mainViewModel.playbackState.observe(this) {
+            playbackState = it
+            binding.ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+
+
+            )
+        }
+        mainViewModel.isConnected.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        binding.rootLayout, result.message ?: "Unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+
+            }
+        }
+
+        mainViewModel.networkError.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        binding.rootLayout, result.message ?: "Unknown error occurred",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                    else -> Unit
+                }
+
+            }
         }
     }
 
